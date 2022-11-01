3e8804ed5470caf9858b8b50824c86b09b4e818b
Justin <justin.kay@inxpress.com>
2022-11-01 14:02:38 -0600

    try adding a runtime host

--- config/runtime.exs
+++ config/runtime.exs
@@ -25,6 +25,7 @@ if config_env() == :prod do
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      url: [host: "lit-waters-24046.fly.dev"],
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
