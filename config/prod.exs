80935a69f349310ca5da1b5b88a6a4abb9e5f45f
Justin <justin.kay@inxpress.com>
2022-11-01 13:28:50 -0600

    change url for fly.io

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :slime_converter, SlimeConverterWeb.Endpoint,
-  url: [scheme: "https", host: "lit-waters-24046.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "lit-waters-24046.fly.dev", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
